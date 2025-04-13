171ec4abf17704740f8cf8f5916e7917ae1558ec
WillsRebo <33318358+WillsRebo@users.noreply.github.com>
2025-04-13 18:18:45 -0300

    feature/css and sections

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const mobileNavList = document.querySelector('.nav-mobile-list');
-  
-    if (menuToggle && mobileNavList) {
-      menuToggle.addEventListener('click', function() {
-        mobileNavList.classList.toggle('open');
+  const menuToggle = document.querySelector('.menu-toggle');
+  const mobileNavList = document.querySelector('.nav-mobile-list');
+  const mobileNavLinks = document.querySelectorAll('.nav-mobile-list .nav-link'); // Selecciona todos los enlaces del menú móvil
+
+  if (menuToggle && mobileNavList) {
+    menuToggle.addEventListener('click', function() {
+      mobileNavList.classList.toggle('open');
+    });
+
+    // Agrega un event listener a cada enlace del menú móvil
+    mobileNavLinks.forEach(link => {
+      link.addEventListener('click', function() {
+        // Si el menú está abierto, lo cerramos
+        if (mobileNavList.classList.contains('open')) {
+          mobileNavList.classList.remove('open');
+        }
       });
-    } else {
-      console.error("No se encontraron el botón de menú o la lista de navegación móvil.");
-    }
-  });
+    });
+  } else {
+    console.error("No se encontraron el botón de menú o la lista de navegación móvil.");
+  }
+});
